cbc76443412553837bae6f8c621f993adbd8cec5
Programista-Cpp <70840444+Programista-Cpp@users.noreply.github.com>
2020-11-16 22:22:31 +0100

    Change from polish names to english.

--- Game.h
+++ Game.h
@@ -1,9 +1,9 @@
 #pragma once
 namespace Player
 {
-	std::string imie = "";
-	std::string nick = "";
-	unsigned short wiek = 0;
+	std::string name	= "";
+	std::string nick	= "";
+	unsigned short age	= 0;
 	enum NATIVECOUNTRY {PL, USA, GB, RU, HU, JP, FR, GER, CZ, ES, CN, MX, BR, NOR, SWE, null} kraj_pochodzenia = null;
 }
 namespace Game
@@ -12,8 +12,7 @@ namespace Game
 	{
 		void Lose()
 		{
-			if (Player::imie[Player::imie.size() - 1] == 'a') std::cout << "Niestety przegralas...\n";
-			else std::cout << "Niestety przegrales...\n";
+			std::cout << "You lose...\n";
 		}
 	}
 	namespace Quiz
@@ -22,7 +21,7 @@ namespace Game
 		char user_ans = '\0';
 		void Question(std::string q, std::string ans_A, std::string ans_B, std::string ans_C, std::string ans_D)
 		{
-			std::cout << q << "\nOdpowiedzi:\n\nA--" << ans_A << "\tB--" << ans_B << "\nC--" << ans_C << "\tD--" << ans_D << "\n\n";
+			std::cout << q << "\nAnswers:\n\nA--" << ans_A << "\tB--" << ans_B << "\nC--" << ans_C << "\tD--" << ans_D << "\n\n";
 		}
 		void Answer(char correct_answer)
 		{
