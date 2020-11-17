ea121a148cf8543db50e4b1769a458d712b6a9d1
Programista-Cpp <70840444+Programista-Cpp@users.noreply.github.com>
2020-11-17 09:45:13 +0100

    Added language packages to Lose().

--- Game.h
+++ Game.h
@@ -4,15 +4,61 @@ namespace Player
 	std::string name	= "";
 	std::string nick	= "";
 	unsigned short age	= 0;
-	enum NATIVECOUNTRY {PL, USA, GB, RU, HU, JP, FR, GER, CZ, ES, CN, MX, BR, NOR, SWE, null} kraj_pochodzenia = null;
+	enum GENDER {MALE, FEMALE, null} gender = null; // null is to initialise the enum. Without initialisation the std::cin >> doesn't work properly
+	enum NATIVECOUNTRY {PL, USA, GB, RU, HU, JP, FR, GER, CZ, ES, CN, MX, BR, POR, NOR, SWE, null} native_country = null;
 }
 namespace Game
 {
 	namespace Events
 	{
-		void Lose()
+		void Lose() requires ((Player::native_country == GB) || (Player::native_country == USA) || (Player::native_country == CN))
 		{
 			std::cout << "You lose...\n";
+		}
+		void Lose() requires (Player::native_country == PL)
+		{
+			if(gender == FEMALE) std::cout << "Niestety przegralas...\n";
+			else std::cout << "Niestety przegrales...\n";
+		}
+		void Lose() requires ((Player::native_country == MX) || (Player::native_country == ES))
+		{
+			std::cout << "Perdiste...\n";
+		}
+		void Lose() requires (Player::native_country == RU)
+		{
+			std::cout << "Ty proigrala...\n";
+		}
+		void Lose() requires (Player::native_country == GER)
+		{
+			std::cout << "Du hast verloren...\n";
+		}
+		void Lose() requires (Player::native_country == CZ)
+		{
+			std::cout << "Prohral jsi...\n";
+		}
+		void Lose() requires (Player::native_country == HU)
+		{
+			std::cout << "Vesztettel...\n";
+		}
+		void Lose() requires (Player::native_country == JP)
+		{
+			std::cout << "Anata ga maketa...\n";
+		}
+		void Lose() requires (Player::native_country == FR)
+		{
+			std::cout << "Tu as perdu...\n";
+		}
+		void Lose() requires ((Player::native_country == POR) || (Player::native_country == BR))
+		{
+			std::cout << "Voce perdeu...\n";
+		}
+		void Lose() requires (Player::natove_country == NOR)
+		{
+			std::cout << "Du tapte...\n";
+		}
+		void Lose() requires (Player::native_country == SWE)
+		{
+			std::cout << "Du forlorade...\n";
 		}
 	}
 	namespace Quiz
